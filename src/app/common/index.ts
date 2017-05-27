f94326c61779b11b154e0102a6b7c96a44748e61
Michal Michalczuk <michalczukm@gmail.com>
2017-05-27 03:02:13 +0200

    10-stickers effect

--- src/app/common/index.ts
+++ src/app/common/index.ts
@@ -1,17 +1,21 @@
 import { cart, CartStore } from './cart.reducer';
 import { combineReducers, Action, ActionReducer } from '@ngrx/store';
+import { stickers, StickersStore } from './stickers.reducer';
 
 export interface RootStore {
   cart: CartStore;
+  stickers: StickersStore;
 }
 
 export const createRootReducer = (state: any, action: any): ActionReducer<any> => {
   return combineReducers({
     cart: cart,
+    stickers: stickers
   })(state, action);
 };
 
 export * from './cart.reducer';
+export * from './stickers.reducer';
 
 // queries
 const itemsNumberQuery = (store: RootStore) => store.cart.stickers.length + store.cart.tshirts.length;
--- src/app/stickers/effects/stickers.effects.ts
+++ src/app/stickers/effects/stickers.effects.ts
@@ -4,10 +4,16 @@ import { Action } from '@ngrx/store';
 import { Effect, Actions } from '@ngrx/effects';
 import { Sticker } from '../sticker.model';
 import { StickersService } from '../stickers.service';
+import { StickersFetchedAction } from '../../common';
 
 @Injectable()
 export class StickerEffects {
-    // @Effect()
+    @Effect()
+    get$: Observable<Action> = this.actions$.ofType('STICKERS_FETCH_STICKERS')
+        .flatMap(() => {
+            return this.stickersService.getAll()
+                .map((result: Sticker[]) => new StickersFetchedAction(result));
+        });
 
     constructor(private actions$: Actions, private stickersService: StickersService) {
     }
