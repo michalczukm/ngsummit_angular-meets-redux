ca257d002a58895cec5bbea98dd978b915a923a7
Michal Michalczuk <michalczukm@gmail.com>
2017-05-27 03:15:02 +0200

    11 fetch stickers with effects

--- src/app/stickers/stickers.component.ts
+++ src/app/stickers/stickers.component.ts
@@ -6,22 +6,19 @@ import { Store } from '@ngrx/store';
 import { CartService } from '../cart/cart.service';
 import { RootStore } from '../common/index';
 import { StickerAddAction } from '../common/cart.reducer';
+import { FetchStickersAction } from '../common/stickers.reducer';
 
 @Component({
   selector: 'smt-stickers',
   templateUrl: './stickers.component.html',
   styleUrls: ['./stickers.component.css']
 })
-export class StickersComponent implements OnInit {
+export class StickersComponent {
   stickers$: Observable<Sticker[]>;
 
-  constructor(
-    private stickersService: StickersService,
-    private store: Store<RootStore>) {
-  }
-
-  ngOnInit() {
-    this.stickers$ = this.stickersService.getAll();
+  constructor(private store: Store<RootStore>) {
+    this.stickers$ = this.store.select((state) => state.stickers.stickers);
+    this.store.dispatch(new FetchStickersAction());
   }
 
   buy(sticker: Sticker) {
